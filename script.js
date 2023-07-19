9231a80a102c6254a3eedf630f3b3e66c787f4bb
prabhat kumar <prabhat.kumar.24.2@mountblue.tech>
2023-07-19 11:22:19 +0530

    javascript added for next button

--- script.js
+++ script.js
@@ -3,6 +3,63 @@
     const Arcade = document.getElementById('Arcade');
     const Advanced = document.getElementById('Advanced');
     const Pro = document.getElementById('Pro');
+    const errorMessage= document.getElementById('error-field')
+
+    const container = document.querySelector(".container")
+    
+    const firstContainer = document.querySelector("#firstContainer");
+    const secondContainer = document.querySelector("#secondContainer");
+    const thirdContainer = document.querySelector("#thirdContainer");
+    const forthContainer = document.querySelector("#forthContainer");
+    const fifthContainer = document.querySelector("#fifthContainer");
+
+    const fstBtnNext = document.getElementById("fstBtnNext");
+    const secBtnNext = document.getElementById("secBtnNext");
+    const thdBtnNext = document.getElementById("thdBtnNext");
+    const forthBtnNext = document.getElementById("forthBtnNext");
+    // const fifthBtnNext = document.getElementById("fifthBtnNext");
+
+//first-btn-nxt
+
+    container.addEventListener("click", function(event){
+
+            console.log(event.target)
+         if(event.target === fstBtnNext)
+         {
+            firstContainer.style.display="none";
+            secondContainer.style.display="block";
+            secondContainer.style.display="flex";
+
+         }
+         if(event.target === secBtnNext)
+         {
+            secondContainer.style.display="none";
+            thirdContainer.style.display="block";
+            thirdContainer.style.display="flex";
+
+         }
+         if(event.target === thdBtnNext)
+         {
+            thirdContainer.style.display="none";
+            forthContainer.style.display="block";
+            forthContainer.style.display="flex";
+
+         }
+         if(event.target === forthBtnNext)
+         {
+            forthContainer.style.display="none";
+            fifthContainer.style.display="block";
+            fifthContainer.style.display="flex";
+
+         }
+         if(event.target === fstBtnNext)
+         {
+            firstContainer.style.display="none";
+            secondContainer.style.display="block";
+            secondContainer.style.display="flex";
+
+         }
+    })
 
     checkbox.addEventListener('change', function() {
       if (checkbox.checked) {
@@ -21,10 +78,20 @@
         const paraPro = document.createElement('p');
         paraPro.innerHTML = '2 months free';
         Pro.appendChild(paraPro);
-        
+
       } else {
         Arcade.textContent = '$9/mo';
         Advanced.textContent = '$12/mo';
         Pro.textContent = '$15/mo';
       }
+
+    //   formValidate()
     });
+
+
+    function formValidate()
+    {
+        const name=document.querySelector('#name').value;
+        const email=document.querySelector('#email').value;
+        const number = document.querySelector('#number').value;
+    }
